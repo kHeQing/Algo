ea1ba5757531405a4d63a587c1e4e294e51dfad0
HeQing <heqing5@tal.com>
2021-12-09 11:03:55 +0800

    feat：更新

--- Algo/main.swift
+++ Algo/main.swift
@@ -10,4 +10,24 @@ import Foundation
 
 print("Hello, World!")
 
-print(SortedSquares().sortedSquares([-3,-2,0,1,5]))
+var arr = [6,3,9,2,10,4]
+Quick().quick(&arr)
+print(arr)
+
+
+func maxLength ( _ arr: [Int]) -> Int {
+    guard arr.count > 2 else {
+        return arr.count
+    }
+    var windows = [Int: Int]()
+    var res = 0
+    var l = -1
+    for r in 0..<arr.count {
+        if (windows.keys.contains(arr[r])) {
+            l = max(l, windows[arr[r]]!)
+        }
+        res = max(res, r-l)
+        windows[arr[r]] = r
+    }
+    return res
+}
